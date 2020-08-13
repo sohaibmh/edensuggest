7fe74d12c320dc2f18fb0a496927ed0c37196254
Sohaib Mubashir <sohaibmh@yahoo.com>
2020-08-13 22:46:38 +0100

    added some amazon products suggestions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>My top Amazon products/services:</h1>
+        <ul className="productsList">
+          <li>
+            <h3>Audible</h3>There’s only one word for this: love! Audible is an
+            extremely good product that allows me to listen to my favourite
+            books. I am a slow reader and get bored quickly when I read a book.
+            And when reading a book I have to dedicate all my attention into
+            reading that book – i.e. I’m not able to multitask. But when it
+            comes to listening a book, I’m able to adjust the listening speed, I
+            can listen to the book when I’m working out, or when I’m driving etc
+            etc! I love it and I would recommend it to every single person! The
+            great thing is that you can get a free 30 day trial.{" "}
+          </li>
+          <li>
+            <h3>Sanicloth UNXPOO159 70 Alcohol Wipes</h3>So I used to work in a
+            hospital lab where we had this product available for us to clean our
+            desks, pipettes, vortex, and any other equipment, you name it. But
+            when I would go home I would yearn for this as I was so used to it
+            at work. So I searched it on Amazon, and to my delight, it was
+            available and I immediately bought it. It’s really good and I would
+            use it to disinfect anything!
+          </li>
+          <li>
+            <h3>V55 MAX Double Strength Salicylic Acid Cream</h3> This is
+            absolutely great for any person that gets spots on their face. I
+            would get spots very frequently no matter how old I grew! I came
+            across this product and it absolutely changed everything! The
+            results are brilliant – now months go by and I wouldn’t get a single
+            spot on my face. One thing you should keep in mind is that you will
+            not get the desired results immediately after applying. You have to
+            consistently apply it for two or three weeks. And then you will know
+            what I’m talking about!
+          </li>
+        </ul>
       </header>
     </div>
   );
